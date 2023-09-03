3c73342acadf145a82ffb6625bac212520405df6
Petro Mozil <mozil.petryk@gmail.com>
2023-09-03 14:00:38 +0300

    Further refactor haskell code

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,13 +1,14 @@
 module Main where
 
-mullerSequence :: [(Rational, Rational)]
-mullerSequence = iterate fn (4.25::Rational, 4::Rational)
-    where
-        fn (a1, a2) = (108 - (815 - (1500 / a2)) / a1, a1)
+mullerSequence :: [Float]
+mullerSequence = 
+    map (fromRational . fst) $
+    iterate fn (4.25::Rational, 4::Rational)
+        where
+            fn (a1, a2) = (108 - (815 - (1500 / a2)) / a1, a1)
 
 main :: IO ()
 main = putStrLn $
     unlines $
     map show $
-    map (\(x, _) -> fromRational x) $ 
     take 100 mullerSequence
